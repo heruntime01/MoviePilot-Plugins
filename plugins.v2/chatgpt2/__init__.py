6c6e0edd700bc29928fbfbd9c77621efaf69e8a8
heruntime01 <heruntime@163.com>
2025-02-17 17:17:33 +0800

    1.3

--- plugins.v2/chatgpt2/__init__.py
+++ plugins.v2/chatgpt2/__init__.py
@@ -16,7 +16,7 @@ class ChatGPT2(_PluginBase):
     # 插件图标
     plugin_icon = "Chatgpt_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "heruntime01"
     # 作者主页
